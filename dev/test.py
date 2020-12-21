86a033a8d50cb958a538cfb3a0caf132b06a580c
zarzouram <muhammadzarzoura@gmail.com>
2020-12-21 22:44:20 +0100

    test.py

--- dev/test.py
+++ dev/test.py
@@ -16,39 +16,45 @@ def plot_tree(g):
 
 # %%
 text = """First of all, through cooperation, children can learn about interpersonal skills which are significant in the future life of all students."""
-s1 = list(range(10, 20))
-s2 = list(range(26, 46))
-s1.extend(s2)
+s1 = list(range(24))
 dep = prep.DEPENDENCYDGL(text, s1)
 
 # %%
 plot_tree(dep.g.to_networkx())
 
 # %%
-g = dgl.graph(([1, 2, 3, 4, 6, 5, 7], [0, 0, 2, 2, 3, 4, 4]))
-plot_tree(g.to_networkx())
+# g = dgl.graph(([1, 2, 3, 4, 6, 5, 7], [0, 0, 2, 2, 3, 4, 4]))
+# plot_tree(g.to_networkx())
 
 
 # %%
 def message_func(edges):
+    print("MF: ", edges.src["_ID"])
     return {'h': edges.src['h'], 'c': edges.src['c']}
 
 
 def reduce_func(nodes):
-    h_cat = nodes.mailbox['h']
-    c_cat = nodes.mailbox['c']
-    return {'iou': c_cat - h_cat, 'c': c}
+    print("RF: ", nodes.data["_ID"])
+    print()
+    h = nodes.mailbox['h'].view(nodes.mailbox['h'].size(0), -1)
+    c = nodes.mailbox['c'].view(nodes.mailbox['c'].size(0), -1)
+    return {'h': h, 'c': c}
 
 
 def apply_node_func(nodes):
+    print("AF: ", nodes.data["_ID"])
     c = nodes.data["c"]
     h = nodes.data["h"]
-    return {h: "h", c: "c"}
+    return {'h': h, 'c': c}
 
 
 # %%
-g = dgl.graph(g.edges())
-g.ndata["type"] = th.tensor([0, 0, 1, 0, 1, 1, 0, 1], dtype=th.float32)
+g = dgl.graph(dep.g.edges())
+g.ndata["type"] = th.tensor([   0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0,
+                                0, 1, 1, 1, 1,
+                                1, 1, 1, 0], dtype=th.float32)
 n = g.number_of_nodes()
 h = []; c = []
 for i in range(n):
@@ -57,9 +63,13 @@ h = th.tensor(h, dtype=th.float32)
 c = th.tensor(c, dtype=th.float32)
 g.ndata["h"] = h
 g.ndata["c"] = c
+
 g.ndata["iou"] = th.zeros((n, 3))
 
 dgl.prop_nodes_topo(g,
                     message_func=message_func,
                     reduce_func=reduce_func,
-                    apply_node_func=apply_node_func)
+                    apply_node_func=apply_node_func
+                    )
+
+# %%
