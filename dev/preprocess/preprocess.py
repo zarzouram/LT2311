3d6a221afb184e977c0bdf8630d109fe0bdd534d
zarzouram <muhammadzarzoura@gmail.com>
2020-12-23 20:29:00 +0100

    treeLSTM_second_attempt

--- dev/preprocess/preprocess.py
+++ dev/preprocess/preprocess.py
@@ -1,39 +1,80 @@
 import spacy
 import torch as th
+import networkx as nx
 import dgl
 # import matplotlib.pyplot as plt
 
 
-class DEPENDENCYDGL():
-    def __init__(self, text, comp_idxs, device="cpu"):
-        # parse text
-        self.device = device
+class DependencyDGL():
+    def __init__(self, text, word2id: dict, graph_type=0, device="cpu"):
+        if device == "cpu":
+            self.device = th.device('cpu')
+        else:
+            self.device = device
+
+        # NOTE:
+        # ## EITHER PARSE OR RECONSTRUCT ## #
         self.text_parsed = self.__dep_parse(text)
-        self.g = self.__dep_graph(comp_idxs)
+        # convert dependency output to graph
+        self.g = self.__dep_graph(graph_type, word2id)
 
     def __dep_parse(self, text):
+        # parse using Spacy.
+        # TODO: give the ability to parse using external function.
         nlp = spacy.load("en_core_web_sm")
         doc = nlp(text)
         return doc
 
-    def __dep_graph(self, comp_idxs):
-        # source node, destination node, node type (u1, v1, n_type)
-        u1 = []; v1 = []; n_type = []
+    def __dep_graph(self, graph_type: int, word2id: dict):
+        # source node, token: u1
+        # destination node, token head: v1
+        u1 = []; v1 = []; roots = []; token_id = []
 
+        # construct graph u_i -> v_i
         for token in self.text_parsed:
-            # node type = 1 if token in argument component else 0
-            n_type.append(int(token.i in comp_idxs))
-
+            token_id.append(word2id[token.text])
             if token.dep_ == "ROOT":
-                continue
+                roots.append(token.i)
             else:
-                # Forward path: bottom-up = leaves-root
+                # Forward path:
+                # bottom-up = leaves-root (word->head)
                 u1.append(token.i)
                 v1.append(token.head.i)
 
-        g_lr = dgl.graph((u1, v1), device=self.device)  # leaves to root
-        g_lr.ndata["type"] = th.tensor(n_type, dtype=th.uint8)
+        # connect multiple graphs to make one graph
+        # TODO:
+        # add multiple ways to construct the document tree from sentences tree
+        if graph_type == 0 and len(roots) > 1:
+            u1.extend(roots[:-1])
+            v1.extend(roots[1:])
+
+        # graph from leaves to root direction
+        g_lr = dgl.graph((u1, v1), device=self.device)
+        # add vocab word_id as an attribute to each node
+        g_lr.ndata["word"] = th.tensor( token_id,
+                                        dtype=th.long,
+                                        device=self.device)
+        n = g_lr.number_of_nodes()
+        g_lr.ndata["type_n"] = th.zeros((n, 1))
         return g_lr
+
+    def get_shortest(self, start_n, end_n):
+        # get the shortest path between two nodes
+        # convert dgl to networks graph
+        g = self.g.to_networkx().to_undirected()
+        thepath = nx.shortest_path(g, source=start_n, target=end_n)
+        return thepath
+
+    def __change_nodes_type(self, nodes):
+        n = nodes.data["type_n"].size()[0]
+        type_nodes = th.ones((n, 1))
+        return {"type_n": type_nodes}
+
+    def graph_process(self, idx: list, pross_type=0):
+        if pross_type == 0:  # test
+            nodes_shortest = self.get_shortest(idx[0], idx[1])
+            self.g.apply_nodes( func=self.__change_nodes_type,
+                                v=nodes_shortest)
 
 
 if __name__ == "__main__":
@@ -43,4 +84,4 @@ if __name__ == "__main__":
     s1 = list(range(10, 20))
     s2 = list(range(26, 46))
     s1.extend(s2)
-    dep = DEPENDENCYDGL(text, s1)
+    dep = DependencyDGL(text, s1)
--- dev/test.py
+++ dev/test.py
@@ -1,9 +1,11 @@
 # %%
 import networkx as nx
+import spacy
 import dgl
 from preprocess import preprocess as prep
 import matplotlib.pyplot as plt
 import torch as th
+import torch.nn as nn
 
 
 # %%
@@ -15,61 +17,166 @@ def plot_tree(g):
 
 
 # %%
-text = """First of all, through cooperation, children can learn about interpersonal skills which are significant in the future life of all students."""
-s1 = list(range(24))
-dep = prep.DEPENDENCYDGL(text, s1)
+class Vocabulary:
+    # map word to ints
+    def __init__(self, start=0):
+        self.vocab = {}
+        self.reverse_vocab = {}
+        self.__next = start
 
-# %%
-plot_tree(dep.g.to_networkx())
+    def add(self, word):
+        # Add word to the vocabulary dict
+        if word not in self.vocab:
+            self.vocab[word] = self.__next
+            self.reverse_vocab[self.__next] = word
+            self.__next += 1
+        return self.vocab.get(word)
 
-# %%
-# g = dgl.graph(([1, 2, 3, 4, 6, 5, 7], [0, 0, 2, 2, 3, 4, 4]))
-# plot_tree(g.to_networkx())
+    def __getitem__(self, item):
+        # Get the word id of given word or vise versa
+        if isinstance(item, int):
+            return self.reverse_vocab.get(item, None)
+        else:
+            return self.vocab.get(item, None)
 
-
-# %%
-def message_func(edges):
-    print("MF: ", edges.src["_ID"])
-    return {'h': edges.src['h'], 'c': edges.src['c']}
-
-
-def reduce_func(nodes):
-    print("RF: ", nodes.data["_ID"])
-    print()
-    h = nodes.mailbox['h'].view(nodes.mailbox['h'].size(0), -1)
-    c = nodes.mailbox['c'].view(nodes.mailbox['c'].size(0), -1)
-    return {'h': h, 'c': c}
-
-
-def apply_node_func(nodes):
-    print("AF: ", nodes.data["_ID"])
-    c = nodes.data["c"]
-    h = nodes.data["h"]
-    return {'h': h, 'c': c}
+    def __len__(self):
+        # Get vocabulary size
+        return len(self.vocab)
 
 
 # %%
-g = dgl.graph(dep.g.edges())
-g.ndata["type"] = th.tensor([   0, 0, 0, 0, 0,
-                                0, 0, 0, 0, 0,
-                                0, 0, 0, 0, 0,
-                                0, 1, 1, 1, 1,
-                                1, 1, 1, 0], dtype=th.float32)
+class TreeLSTMCell(nn.Module):
+    def __init__(self, x_size, h_size, N=2):
+        super(TreeLSTMCell, self).__init__()
+        self.W_iou = nn.Linear(x_size, 3 * h_size, bias=False)
+        self.Um1_iou = nn.Linear(h_size, 3 * h_size, bias=False)
+        self.Um2_iou = nn.Linear(h_size, 3 * h_size, bias=False)
+        self.b_iou = nn.Parameter(th.zeros(1, 3 * h_size))
+        self.U_f = nn.Linear(N * h_size, N * h_size)
+
+    def message_func(self, edges):
+        return {'h': edges.src['h'], 'c': edges.src['c']}
+
+    def reduce_func(self, nodes):
+        h_cat = nodes.mailbox['h'].view(nodes.mailbox['h'].size(0), -1)
+        f = th.sigmoid(self.U_f(h_cat)).view(*nodes.mailbox['h'].size())
+        c = th.sum(f * nodes.mailbox['c'], 1)
+        return {'iou': self.U_iou(h_cat), 'c': c}
+
+    def apply_node_func(self, nodes):
+        iou = nodes.data['iou'] + self.b_iou
+        i, o, u = th.chunk(iou, 3, 1)
+        i, o, u = th.sigmoid(i), th.sigmoid(o), th.tanh(u)
+        c = i * u + nodes.data['c']
+        h = o * th.tanh(c)
+        return {'h': h, 'c': c}
+
+
+class Embedings(nn.Module):
+    def __init__(   self,
+                    num_vocabs,
+                    emb_size,
+                    emb_type=None,
+                    pretrained_emb=None):
+        super(Embedings, self).__init__()
+
+        if emb_type is not None and pretrained_emb is None:
+            raise ValueError("missing pretrained embedding \"pretrained_emb\"")
+
+        self.embed = nn.Embedding(num_vocabs, emb_size)
+        if emb_type == "Glove":
+            self.embed.weight.data.copy_(pretrained_emb)
+            self.embed.weight.requires_grad = True
+        else:
+            pass
+
+
+class TreeLSTM(nn.Module):
+    def __init__(   self,
+                    num_vocabs,
+                    x_size,
+                    h_size,
+                    dropout
+                    ):
+        super(TreeLSTM, self).__init__()
+        self.x_size = x_size
+        self.embeddings = Embedings(num_vocabs, x_size)
+        self.dropout = nn.Dropout(dropout)
+        self.cell = TreeLSTMCell(x_size, h_size)
+
+    def forward(self, g, h, c):
+        """Compute tree-lstm prediction given a batch.
+        Parameters
+        ----------
+        batch : dgl.data.SSTBatch
+            The data batch.
+        g : dgl.DGLGraph
+            Tree for computation.
+        h : Tensor
+            Initial hidden state.
+        c : Tensor
+            Initial cell state.
+        Returns
+        -------
+        logits : Tensor
+            The prediction of each node.
+        """
+        embeddings = self.embeddings.embed(g.ndata["word"])
+        g.ndata['iou'] = self.cell.W_iou(self.dropout(embeddings))
+        g.ndata['h'] = h
+        g.ndata['c'] = c
+        # propagate
+        dgl.prop_nodes_topo(g,
+                            message_func=self.cell.message_func,
+                            reduce_func=self.cell.reduce_func,
+                            apply_node_func=self.cell.apply_node_func)
+        # compute logits
+        h = self.dropout(g.ndata.pop('h'))
+        return h
+
+
+# %%
+def dummy_word2id(text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    vocab = Vocabulary()
+    for token in doc:
+        vocab.add(token.text)
+    return vocab.vocab
+
+
+# %%
+text = """From this point of view, I firmly believe that we should attach more importance to cooperation during primary education.
+First of all, through cooperation, children can learn about interpersonal skills which are significant in the future life of all students."""
+
+vocab = dummy_word2id(text)
+# NOTE: What is include in the batch? Will the text be included in the batch? if not, I need to change the preprocess.DependencyDGL to compile the text from word_id (in the vocabulary) to text, then parse the compiled text. Other option is to parse the whole documents during preprocessing and incluse it in the batch.
+dep = prep.DependencyDGL(text, vocab)
+
+# %%
+plot_tree(dgl.to_homogeneous(dep.g).to_networkx())
+print(dep.get_shortest(44, 19))
+
+# %%
+dep.graph_process([19, 44], 0)
+print(dep.g.ndata)
+
+# %%
+g = dep.g
 n = g.number_of_nodes()
-h = []; c = []
-for i in range(n):
-    h.append([i] * 3); c.append([n - i] * 3)
-h = th.tensor(h, dtype=th.float32)
-c = th.tensor(c, dtype=th.float32)
-g.ndata["h"] = h
-g.ndata["c"] = c
+h_size = 5
+emb_size = 6
 
-g.ndata["iou"] = th.zeros((n, 3))
+h = th.zeros((n, h_size))
+c = th.zeros((n, h_size))
 
-dgl.prop_nodes_topo(g,
-                    message_func=message_func,
-                    reduce_func=reduce_func,
-                    apply_node_func=apply_node_func
+model = TreeLSTM(   len(vocab),
+                    emb_size,
+                    h_size,
+                    0.5
                     )
 
+
+# %%
+model(g, h, c)
 # %%
